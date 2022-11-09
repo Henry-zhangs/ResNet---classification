ff12478ccf80afb2888ce31123c23f051d211f9d
Henry <85046712+Joker-123-net@users.noreply.github.com>
2022-11-09 19:14:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ st.write("")
 file_up = st.file_uploader("Upload an image")
 
 if file_up is None:
-    image =r'.\image\img.png'
+    image ='./image/img.png'
     img = Image.open(image)
     st.image(img, caption='Image.', use_column_width=True)
     st.write("")
